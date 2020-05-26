44900b50c246a4a42d8bbebefe5c10e087b735cf
hoangharry <49946656+hoangharry@users.noreply.github.com>
2020-05-26 15:52:49 +0700

    Update evolution.py
    Add cmt

--- evolution.py
+++ evolution.py
@@ -17,13 +17,13 @@ def f_para(obj, new_pop, i):
     new_population = Population()
     front_num = 0
     while len(new_population) + len(obj.population.fronts[front_num]) <= obj.num_of_individuals:
+        print(obj.population.fronts[front_num])
         obj.utils.calculate_crowding_distance(obj.population.fronts[front_num])
         new_population.extend(obj.population.fronts[front_num])
         front_num += 1
     obj.utils.calculate_crowding_distance(obj.population.fronts[front_num])
     obj.population.fronts[front_num].sort(key=lambda individual: individual.crowding_distance, reverse=True)
-    new_population.extend(obj.population.fronts[front_num][0:obj.num_of_individuals-len(new_population)])
-    
+    new_population.extend(obj.population.fronts[front_num][0:obj.num_of_individuals-len(new_population)])    
     new_pop[i] = new_population
 
 class Evolution:
@@ -39,7 +39,8 @@ class Evolution:
 
     def evolve(self):
         self.population = self.utils.create_initial_population()
-        num_thrd = mp.cpu_count()
+        num_thrd = mp.cpu_count() # get number of threads
+        # Divide to subpopulation
         new_pop = [Population()]*num_thrd
         lst = []
         idx = self.utils.num_of_individuals // num_thrd
@@ -52,35 +53,31 @@ class Evolution:
         evo = Evolution(self.utils.problem, num_of_individuals=self.utils.num_of_individuals - tmp)
         evo.population = self.utils.assign_pop(self.population, tmp, len(self.population.population))
         lst.append(evo)
-            
+
+        # Evolution in parallel   
         for gen in range(self.num_of_generations):
+            # Subpopulation evolute
             t = [None]*num_thrd
             id_num = 0
-            pre_best = []
             for evo in lst:
                 t[id_num] = thrd.Thread(target=f_para, args=(evo, new_pop, id_num))
                 t[id_num].start()
                 id_num = id_num + 1
+
             runnerup_individuals = []
             best_individuals = []
+            # Join threads and get the best individuals
             for i in range(num_thrd):
                 t[i].join()
                 self.utils.fast_nondominated_sort(new_pop[i])
-                # if gen > 0:
-                #     if (new_pop[i].fronts[0][0].objectives[0] == pre_best[i]):
-                #         if pre_best[i] == max(pre_best):
-                #             best_individual = new_pop[i].fronts[0][0]
                 runnerup_individuals.extend(new_pop[i].fronts[1:3][0])
                 best_individuals.append(new_pop[i].fronts[0][0])
-            # for i in range(len(t)):
-            #     if best_individual is not None:
-            #         # new_pop[i].extend(best_individual)
-            #         new_pop[i].append(best_individual)
-            #     new_pop[i].extend(sub_individuals[2*i:2*i+2]) 
-            #     self.population.extend(new_pop[i])
+
+            # Add the best individuals from other subpopulations
             for i in range(num_thrd):
-                new_pop[i].extend(runnerup_individuals)
+                new_pop[i].extend(runnerup_individuals) #add runnerup individuals
                 self.utils.fast_nondominated_sort(new_pop[i])
+                # check if the best individual 
                 if new_pop[i].fronts[0][0].objectives[0] == best_individuals[i].objectives[0]:
                     for j in range(num_thrd):
                         if i == j:
@@ -88,6 +85,7 @@ class Evolution:
                         else:
                             new_pop[j].append(best_individuals[i])
 
+            # Copy subpopulations for the next genneration
             for i in range(num_thrd):
                 self.utils.fast_nondominated_sort(new_pop[i])
                 temp_population = Population()
@@ -96,8 +94,13 @@ class Evolution:
                     self.utils.calculate_crowding_distance(new_pop[i].fronts[front_idx])
                     temp_population.extend(new_pop[i].fronts[front_idx])
                     front_idx +=1
+                self.utils.calculate_crowding_distance(new_pop[i].fronts[front_idx])
+                new_pop[i].fronts[front_idx].sort(key=lambda individual: individual.crowding_distance, reverse=True)
+                temp_population.extend(new_pop[i].fronts[front_idx][0:lst[i].num_of_individuals - len(temp_population)])
+                lst[i].population = Population()
                 lst[i].population = temp_population
 
+        # Get the lastest population 
         self.population = Population()
         for pop in new_pop:
             self.population.extend(pop)
